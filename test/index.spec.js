2a019b713bdc667841b6100c280bcbea80ac233e
Wiktor Obrębski <psychowico@gmail.com>
2016-04-11 19:45:00 +0200

    adding mock library, cookie `get` and `set` tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,17 +1,49 @@
 import assert from 'assert'
+import Rx from 'rx'
+import simple from 'simple-mock'
+import cookie from 'cookie_js'
 import {createCookieDriver} from '../src/index'
-import Rx from 'rx';
 
 describe('Cycle.js Cookie driver', function() {
+    const EmptyObservable = Rx.Observable.empty();
     var driver = null;
 
     beforeEach(() => {
-        const EmptyObservable = Rx.Observable.empty();
-        driver = createCookieDriver()(EmptyObservable);
+        driver = createCookieDriver();
+    });
+
+    afterEach(() => {
+        simple.restore();
     });
 
     it('should expose `get` and `all` api', function() {
-        assert.strictEqual(typeof driver.all, 'function');
-        assert.strictEqual(typeof driver.get, 'function');
+        assert.strictEqual(typeof driver(EmptyObservable).all, 'function');
+        assert.strictEqual(typeof driver(EmptyObservable).get, 'function');
+    });
+
+    it('should set cookie value', function(done) {
+        simple.mock(cookie, 'set', (key, val) => {
+            assert.strictEqual(key, 'MyCookie');
+            assert.strictEqual(val, '321');
+            done();
+        });
+        const subject = new Rx.Subject();
+        const cookieDriver = driver(subject);
+        subject.onNext({
+            'key': 'MyCookie',
+            'value': '321'
+        });
+    });
+
+    it('should get cookie value', function(done) {
+        simple.mock(cookie, 'get', (key) => {
+            assert.strictEqual(key, 'MyCookie');
+            return '321';
+        });
+        const cookieDriver = driver(EmptyObservable);
+        cookieDriver.get('MyCookie').subscribe((result) => {
+            assert.strictEqual(result, '321');
+            done();
+        })
     });
 });
